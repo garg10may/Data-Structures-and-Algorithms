5735609154e11511457b38fec3151a665d5cfe3f
Garg <NBKCHJL@asia.bankofamerica.com>
2016-10-25 13:48:06 +0530

    1.0

---
+++ towerOfHanoi.py
@@ -0,0 +1,12 @@
+
+def towerOfHanoi(height, start, temp, target):
+    count =0
+    if height >=1:    
+        count = towerOfHanoi(height-1, start, target, temp)
+        #print ('moving disk from ' + start + ' to ' + target)
+        count +=1
+        count += towerOfHanoi(height-1, temp, start, target)
+    return count
+
+
+print towerOfHanoi(24,"A","B","C")
