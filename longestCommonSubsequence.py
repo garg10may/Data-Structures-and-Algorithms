2abd130491b470cdd181aa151bcbd7ba6973ea13
garg10may <garg10may@gmail.com>
2016-11-03 19:46:09 +0530

    added reconstructing string, but still wrong

--- longestCommonSubsequence.py
+++ longestCommonSubsequence.py
@@ -2,6 +2,8 @@
 # abcd 1abcd1 --> abcd
 # abcd 1ab1c1 --> ab 
 
+
+#recursion with dp
 def LCS(s1, s2, d = {}):
     try:
         return d[(s1,s2)]
@@ -21,7 +23,8 @@ def LCS(s1, s2, d = {}):
 
 print LCS('tanmayanganaabcdef', "elepltanmabananaakjlkjkojhanbcorangejant")
 
-def LCS(s1, s2, d = {}):
+#recursion 
+def LCS(s1, s2):
     if s2.find(s1) != -1:
         return s1
     if s1 == '':
@@ -35,6 +38,7 @@ def LCS(s1, s2, d = {}):
 
 print LCS('tanmayorandaabcdef', "elepltanmabananaakjlkjkojhanbcorangejant")
 
+#The problem remains same, now the sequence 
 # Need not be continuous
 # abcd 1ab1c1 --> abc
 
@@ -58,12 +62,14 @@ def LCS2(s1, s2):
     return largest
 
 
-print LCS2('tanmayorandaabcdef', "anmabananaakjlkjkojhanbcorangej")
+print LCS2('tanmayorandaabcdef', "anmabannbcorangej")
 
+#DP solution, also need to find out the sequence
+#this is wrong
 def LCS3( s1, s2):
     row = len(s1)
     col = len(s2)
-    T = [ [ 0 for i in range(col)] for j in range(row)  ]
+    T = [ [ 0 for _ in range(col)] for _ in range(row)  ]
     for i in (range(1,row)):
         for j in range( 1,col):
             if s1[i-1] == s2[j-1]:
@@ -73,6 +79,38 @@ def LCS3( s1, s2):
                                T[i][j-1] )
     return T
 
-T =  LCS3('tanmayorandaabcdef', "anmabananaakjlkjkojhanbcorangej")
+T =  LCS3('tanmayorandaabcdef', "anmabannbcorangej")
 print T[-1][-1]
 
+def constructSequence(T, s1):
+    result=''
+    i = len(T) -1 #row
+    j = len(T[0]) -1 #col
+    print i, j
+    while i >= 0 and j >= 0:
+        print i, j        
+        if T[i][j] == T[i-1][j-1] +1:
+            result += s1[i]
+        i -= 1
+        j -= 1
+    return result[::-1]
+
+print constructSequence(T, 'tanmayorandaabcdef')
+
+#recursive solution for the same, highly inefficent
+def lcs_recursive_helper(sequence1, sequence2, index1, index2):
+    if (index1 == len(sequence1)) or (index2 == len(sequence2)):
+        return 0
+
+    if sequence1[index1] == sequence2[index2]:
+        return 1 + lcs_recursive_helper(sequence1, sequence2, index1 + 1, index2 + 1)
+
+    return max(lcs_recursive_helper(sequence1, sequence2, index1 + 1, index2),
+               lcs_recursive_helper(sequence1, sequence2, index1, index2 + 1))
+
+
+def longest_common_subsequence_recursive(sequence1, sequence2):
+    return lcs_recursive_helper(sequence1, sequence2, 0, 0)
+
+
+print lcs_recursive_helper('tanmayorand', "anmabananaaan", 0, 0)
