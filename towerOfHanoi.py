c46c6115ddff43da42efc1e288cfef7cdb7418c2
garg10may <garg10may@gmail.com>
2016-11-03 19:47:01 +0530

    minor

--- towerOfHanoi.py
+++ towerOfHanoi.py
@@ -3,10 +3,10 @@ def towerOfHanoi(height, start, temp, target):
     count =0
     if height >=1:    
         count = towerOfHanoi(height-1, start, target, temp)
-        #print ('moving disk from ' + start + ' to ' + target)
+        print ('moving disk from ' + start + ' to ' + target)
         count +=1
         count += towerOfHanoi(height-1, temp, start, target)
     return count
 
 
-print towerOfHanoi(24,"A","B","C")
+print towerOfHanoi(4,"A","B","C")
