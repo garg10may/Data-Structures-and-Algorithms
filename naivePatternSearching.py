a386126279908098437d79fa16bfa97beaa3ef61
Garg <garg10may@gmail.com>
2016-12-13 16:37:57 +0530

    minor change

--- naivePatternSearching.py
+++ naivePatternSearching.py
@@ -5,25 +5,14 @@ that prints all occurrences of pat[] in txt[].
 You may assume that n > m.
 '''
 
-import hashlib
-# looks simple and good, but fails in second case, where it should continue
-# from if left off
-def find(s1, s2):
-	size = len(s1)
-	for i in range( len(s2) ):
-		if hashlib.sha512(s2[i : i + size]).hexdigest() == haslib.sha512(s1).hexdigest():
-			print 'Pattern found at index %s'%(i)
 
-#find('AABA', "AABAACAADAABAAABAA")
-print 
-#find('AAAAA', 'AAAAAAAAAAAAAAAAAAAA')
-print
-
-# This looks fine, worst case ~O(2nm) , (m(matching) + m(slicing)) * n
+# This looks fine, worst case ~O(2nm) , O((m+m) * (n-m+1))
+# m+m --> for slicing and comparision
+# n-m+1 --> number of times the loop will
 def find(s1, s2):
 	size = len(s1)
 	index = 0 
-	while ( index != len(s2)):
+	while ( index <= len(s2)):
 		if s2[index : index + size] == s1:
 			print 'Pattern found at index %s'%(index)
 			index += size
@@ -34,11 +23,14 @@ find('AABA', "AABAACAADAABAAABAA")
 print 
 find('AAAA', 'BAAAAAAAAAAAAAAAAAB')
 print 
-find('ABABAC' ,'ABABABCABABABCABABABC')
+find("ABABCABAB" ,'ABABDABACDABABCABAB')
 print '*'*80
+
 
 #Found on geeks for geeks, not sure what they are trying to achieve
 # Python program for Naive Pattern Searching
+# this is also wrong since for second test, it doesn't continue where it left off
+# though this is subjective
 def search(pat, txt):
     M = len(pat)
     N = len(txt)
@@ -54,8 +46,23 @@ def search(pat, txt):
             print "Pattern found at index " + str(i)
 
 print 
-find('AABA', "AABAACAADAABAAABAA")
+search('AABA', "AABAACAADAABAAABAA")
 print 
-find('AAAA', 'BAAAAAAAAAAAAAAAAAB')
+search('AAAA', 'BAAAAAAAAAAAAAAAAAB')
 print 
-find("ABABCABAB" ,'ABABDABACDABABCABAB')
+search("ABABCABAB" ,'ABABDABACDABABCABAB')
+
+'''
+# looks simple and good, but fails in second case, where it should continue
+# from if left off
+def find(s1, s2):
+	size = len(s1)
+	for i in range( len(s2) ):
+		s2[i : i + size] == s1
+			print 'Pattern found at index %s'%(i)
+
+#find('AABA', "AABAACAADAABAAABAA")
+print 
+#find('AAAAA', 'AAAAAAAAAAAAAAAAAAAA')
+print
+'''
