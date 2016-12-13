01271c4bc2d39f32ec190ecdab6cf40aabe32594
Garg <garg10may@gmail.com>
2016-12-13 21:03:55 +0530

    Added rolling hash algorithm explanation

--- rabinKarp.py
+++ rabinKarp.py
@@ -1,5 +1,19 @@
-#Rabin karp algorithm for string searching
-# remember '^' : is exclusive-or (bitwise) not power operator
+#Rabin karp algorithm for string searching 
+# remember '^' : is exclusive-or (bitwise) not power operator in python
+
+'''
+The algorithm for rolling hash user here is as below. Pattern : 'abc', Text : 'abcd'
+Consider 'abc' it's hash value is calcualted using formula a*x^0 + b*x^1 + c*x^2, where x is prime
+Now when we need to roll( i.e. find hash of 'bcd'), which should be b*x^0 + c*x^1 + d*x^2, 
+instead of calculating it whole , we just subract first character value and add next character value. 
+Value of first character would be a*X^0 --> a, so we subract a and divide by prime so that new position becomes
+b*x^0 + c*x^1, now we need to add next character, which would be 'd'*x^(patternLength-1), so here 'd'*x^2 so it becomes
+b*x^0 + c*x^1 + d*x^2, which is what we need. It's O(1) so very useful for long strings. 
+
+Note:
+Here it's recommended to use high value of text characters, therefore use ASCII value for text not just 0,1,2
+Also use prime number greater than 100, so that the hash function is good and less collisions are there
+'''
 
 def hashValue(pattern, prime):
 	value = 0
