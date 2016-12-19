17017dde8f4062501a925422e11b9ef79ec80506
Garg <garg10may@gmail.com>
2016-12-19 20:35:31 +0530

    added comments

--- permutationsOfString.py
+++ permutationsOfString.py
@@ -1,6 +1,9 @@
 
 
-
+#backtracking method, 
+# we swap a with each character, in first iteration we swap with a itself
+# likewise in second we swap with b, now once done, we revert it back and then 
+# in third iteration swap it with c
 def find_permutations( x ):
 
 	n = len(x)
@@ -19,13 +22,14 @@ def find_permutations( x ):
 
 	permute(a, 0, n-1)
 
+
+#we take each character one by one, and then find all the permutations of remaining
+# the trick is in s[:i] + s[i+1]:len(s)], this removes the ith character in each iteration
 def getPermutation(s, prefix=''):
         if len(s) == 0:
                 print prefix,
         for i in range(len(s)):
                 getPermutation(s[0:i] + s[i + 1 : len(s)], prefix + s[i] )
-
-
 
 find_permutations('abc')
 
@@ -35,6 +39,7 @@ getPermutation('abc')
 
 print 
 print
+# to check for how that splitting of character is working
 s = 'abcd'
 for i in range(len(s)):
 	print s[0:i] + s[i + 1 : len(s)]
