f3b4aaf9772cfcea48018aeef8139e178a244b31
Garg <garg10may@gmail.com>
2016-12-19 19:36:59 +0530

    1.0

--- interviewQuestions.py
+++ interviewQuestions.py
@@ -7,16 +7,3 @@ Output: 16, 11, 6, 1, -4, 1, 6, 11, 16
 Input: n = 10
 Output: 10, 5, 0, 5, 10
 '''
-
-n = 16 
-count = 1
-while count != 5:
-	print n, 
-	n -= 5 
-	count +=1 
-count = 1
-while count != 6:
-	print n, 
-	n += 5
-	count +=1 
-
