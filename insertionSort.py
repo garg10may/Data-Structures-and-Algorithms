6d411135b0dbe73626ab739e6cf82f8e18e7c185
Garg <garg10may@gmail.com>
2016-12-22 18:19:55 +0530

    strill wrong, added insert logic

--- insertionSort.py
+++ insertionSort.py
@@ -3,14 +3,25 @@
 #insertion sort, it's just like you sort a hand of cards
 
 
+def insert( listOfElements, element):
+	#insert element in list in sorted order where list is already sorted
+	print listOfElements, element
+	status = 0 
+	for i in range(len(listOfElements)):
+		if element < listOfElements[i] :
+			status = 1 
+			listOfElements.insert( i, element )
+			break
+	if status ==0:
+		listOfElements.append(element)
+	return listOfElements
+
+
 def insertionSort( x ):
 	n = len(x)
 	for i in range(n):
-		for j in range(n):
-			if x[i] < x[j]:
-				temp = x[i]
-				x[j+1:] = x[j:]
-				x[j] = temp
+		insert( x, x.pop())
+
 	return x
 
 print insertionSort( [9,7,6,15,16,5,10,11])
