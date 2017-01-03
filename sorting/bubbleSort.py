f2feaf1270d11b6cd3fedd7f5809d2de37638dc1
Garg <garg10may@gmail.com>
2017-01-03 21:29:24 +0530

    minor

--- sorting/bubbleSort.py
+++ sorting/bubbleSort.py
@@ -2,19 +2,14 @@
 
 # simplest, run two loops keep on comparing and swapping, worst also
 
-
-
 def bubbleSort( x ):
 
 	for i in range(len(x)):
-
 		for j in range(len(x) -i-1):
 
 			if x[j] > x[j+1]:
-				#swap
 				x[j], x[j+1] = x[j+1], x[j]
-
+				
 	return x
-
 
 print bubbleSort( [90,89,1,5,4,8,2])
