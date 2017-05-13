d164297abf92911021584097e8f2c8fbea9599b2
garg10may <garg10may@gmail.com>
2017-05-13 16:34:15 +0530

    added top up

--- dynamic/totalWays.py
+++ dynamic/totalWays.py
@@ -3,12 +3,10 @@ Given a distance 'dist', count total number of ways to cover the distance with 1
 '''
 
 
-
+#Top down
 def totalSteps(n, d= {}):
-
 	global count
 	count +=1
-
 	try:
 		return d[n]
 	except:
@@ -16,12 +14,30 @@ def totalSteps(n, d= {}):
 			return 0
 		if n == 0:
 			return 1
-
 		else:
 			d[n] = (totalSteps(n-1) + totalSteps(n-2) + totalSteps(n-3) )
 			return d[n]
 
-
 count = 0
-print totalSteps(500)
+print totalSteps(10)
+print 'No. of calls %s'%(count)
+
+def totalSteps(n):
+
+	global count
+	count += 1
+
+	d= {}
+	d[0] = 1
+	d[1] = 1
+	d[2] = 2
+
+	for i in range(3,n+1):
+		d[i] = d[i-1] + d[i-2] + d[i-3]
+
+
+	return d[n]
+
+count  =0
+print totalSteps(10)
 print 'No. of calls %s'%(count)
